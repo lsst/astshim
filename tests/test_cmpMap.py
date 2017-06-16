8be2076a946c44d6175b169a0c4f216c8c3ce858
Russell Owen <rowen@uw.edu>
2017-06-16 10:31:33 -0700

    Add test for DM-10946
    Test that a non-square matrix map followed by a shift map
    can be simplified and performs as expected.

--- tests/test_cmpMap.py
+++ tests/test_cmpMap.py
@@ -80,6 +80,34 @@ class TestCmpMap(MappingTestCase):
         cmtopos = cmpmap.applyForward(indata)
         assert_allclose(cmtopos, pred_outdata)
 
+    def test_SeriesMapMatrixShiftSimplify(self):
+        """Test that a non-square matrix map followed by a shift map can be simplified
+
+        This is ticket DM-10946
+        """
+        m1 = 1.0
+        m2 = 2.0
+        shift = 3.0
+        matrixMap = astshim.MatrixMap(np.array([[m1, m2]]))
+        self.assertEqual(matrixMap.nIn, 2)
+        self.assertEqual(matrixMap.nOut, 1)
+        shiftMap = astshim.ShiftMap([shift])
+        seriesMap = matrixMap.then(shiftMap)
+
+        indata = np.array([
+            [1.0, 2.0, 3.0],
+            [0.0, 1.0, 2.0],
+        ], dtype=float)
+        pred_outdata = m1 * indata[0] + m2 * indata[1] + shift
+        pred_outdata.shape = (1, len(pred_outdata))
+
+        outdata = seriesMap.applyForward(indata)
+        assert_allclose(outdata, pred_outdata)
+
+        simplifiedMap = seriesMap.simplify()
+        outdata2 = simplifiedMap.applyForward(indata)
+        assert_allclose(outdata2, pred_outdata)
+
 
 if __name__ == "__main__":
     unittest.main()
