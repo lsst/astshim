5fc3dac3ca099f0f5f4003b919951d07268c67ff
Russell Owen <rowen@uw.edu>
2018-05-29 12:55:46 -0700

    Remove a line of unused data from test_zoomMap

--- tests/test_zoomMap.py
+++ tests/test_zoomMap.py
@@ -28,7 +28,6 @@ class TestZoomMap(MappingTestCase):
                     [1.0, 2.0, -6.0, 30.0, 1.0],
                     [3.0, 99.0, -5.0, 21.0, 0.0],
                     [-5.0, 3.0, -7.0, 37.0, 0.0],
-                    [7.0, -23.0, -3.0, 45.0, 0.0],
                 ], dtype=float)[0:nin]
                 self.checkRoundTrip(zoommap, indata)
                 self.checkMappingPersistence(zoommap, indata)
